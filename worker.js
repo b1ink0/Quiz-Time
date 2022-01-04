53ac9521bc9378d6df520c7e1cc576ead7f272a0
stupidgaming <loginfor357@gmail.com>
2022-01-04 20:12:54 +0530

    Updates

--- worker.js
+++ worker.js
@@ -21,8 +21,8 @@ var urlsToCache = [
   "/Quiz-Time/static/media/rank_1.9cdeb73b.svg",
   "/Quiz-Time/static/media/rank_2.3e55031c.svg",
   "/Quiz-Time/static/media/rank_3.4d4a08b6.svg",
-  "/Quiz-Time/static/css/main.1888ad0d.chunk.css",
-  "/Quiz-Time/static/css/main.1888ad0d.chunk.css.map",
+  // "/Quiz-Time/static/css/main.1888ad0d.chunk.css",
+  // "/Quiz-Time/static/css/main.1888ad0d.chunk.css.map",
 ];
 
 // Install a service worker
